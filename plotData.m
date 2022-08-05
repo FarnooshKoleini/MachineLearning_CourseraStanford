2394ee317bce4f7bb32fd4fc4ea7ac0728e0c584
Farnoosh Koleini <99754293+FarnooshKoleini@users.noreply.github.com>
2022-08-05 18:58:39 -0400

    Add files via upload

--- plotData.m
+++ plotData.m
@@ -1,27 +1,57 @@
-function plotData(x, y)
-%PLOTDATA Plots the data points x and y into a new figure 
-%   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
-%   population and profit.
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
-figure; % open a new figure window
+% Create New Figure
+figure; hold on;
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Plot the training data into a figure using the 
-%               "figure" and "plot" commands. Set the axes labels using
-%               the "xlabel" and "ylabel" commands. Assume the 
-%               population and revenue data have been passed in
-%               as the x and y arguments of this function.
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
 %
-% Hint: You can use the 'rx' option with plot to have the markers
-%       appear as red crosses. Furthermore, you can make the
-%       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
+%
+% separate positive and negative results
 
-plot(x, y, 'rx', 'MarkerSize', 10);
-ylabel('Profit in $10,000s');
-xlabel('Population of city in 10,000s');
+positive = find(y==1)
+negative = find(y==0)
+
+% creat New Figure
+figure;
+
+% plotting positive results on 
+% X_axis: Exam1 Score = X(positive,1)
+% Y_axis: Exam2 Score = X(positive,2)
+
+plot(X(positive,1),X(positive,2),'g+');
+
+% To keep above plotted graph as it is
+
+hold on;
+
+% plotting negative results on
+
+plot(X(negative,1),X(negative,2),'ro');
+
+hold off;
+
+End
 
 
 
-% ============================================================
+
+
+
+
+
+
+
+
+% =========================================================================
+
+
+
+hold off;
 
 end
